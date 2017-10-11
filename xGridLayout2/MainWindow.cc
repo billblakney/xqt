aa0ce763f3ba26cc6fa016f008d9765bed97a58c
billblakney <b.blakney@cox.net>
2017-10-11 14:11:16 -0400

    first change

--- xGridLayout2/MainWindow.cc
+++ xGridLayout2/MainWindow.cc
@@ -35,6 +35,7 @@ void MainWindow::onSend()
 //-----------------------------------------------------------------------------
 void MainWindow::setupView()
 {
+#ifdef OLD
   std::cout << "setupView" << std::endl;
 
   _checkBox = new QCheckBox("CheckBox");
@@ -74,4 +75,69 @@ void MainWindow::setupView()
 //  resize(200,120);
 
   setLayout(tBoxLayout);
+#else
+  std::cout << "setupView" << std::endl;
+
+  const int ROWS = 6;
+  const int COLS = 10;
+  QLabel *tGrid[ROWS][COLS];
+
+  for (int i = 0; i < ROWS; i++)
+    for (int j = 0; j < COLS; j++)
+    {
+      char buff[100];
+      sprintf(buff,"hello %d,%d",i,j);
+//      tGrid[i][j].setText(QString(buff));
+      tGrid[i][j] = new QLabel(buff);
+    }
+
+
+//  _checkBox = new QCheckBox("CheckBox");
+//
+//  _label1 = new QLabel("SomeReallyReallyLongLabel",this);
+//  _lineEdit1 = new QLineEdit();
+//
+//  _label2 = new QLabel("Label",this);
+//  _lineEdit2 = new QLineEdit();
+
+  QGridLayout *tGridLayout = new QGridLayout(this);
+
+  for (int i = 0; i < ROWS; i++)
+    for (int j = 0; j < COLS; j++)
+    {
+      tGridLayout->addWidget(tGrid[i][j],i,j);
+    }
+//  tGridLayout->setRowStretch(1,0);
+//  tGridLayout->setRowStretch(2,0);
+//
+//  tGridLayout->addWidget(_checkBox,0,0);
+//  tGridLayout->addWidget(_label1,1,0);
+//  tGridLayout->addWidget(_label2,2,0);
+//
+//  tGridLayout->addWidget(_lineEdit1,1,1);
+//  tGridLayout->addWidget(_lineEdit2,2,1);
+//
+//  QWidget *tEditorsWidget = new QWidget(this);
+//  tEditorsWidget->setLayout(tGridLayout);
+//
+//
+//  QPushButton *tSendButton = new QPushButton(QString("Send"),this);
+//  tSendButton->resize(100, 30);
+//
+//  QObject::connect( tSendButton, SIGNAL(clicked()), this, SLOT(onSend()) );
+
+  QWidget *tEditorsWidget = new QWidget(this);
+  tEditorsWidget->setLayout(tGridLayout);
+
+  QVBoxLayout *tBoxLayout = new QVBoxLayout(this);
+//  tBoxLayout->setStretch(0,0);
+//  tBoxLayout->setStretch(1,1);
+//  tBoxLayout->addWidget(tEditorsWidget);
+//  tBoxLayout->addWidget(tSendButton);
+  tBoxLayout->addWidget(tEditorsWidget);
+
+//  resize(200,120);
+
+  setLayout(tBoxLayout);
+#endif
 }
