df792dd0d64b2082f7ddc410d6feb6807647a33d
billblakney <spam.blakney@cox.net>
2018-02-03 22:43:17 -0500

    simple cleanup

--- xImage/MainWindow.cc
+++ xImage/MainWindow.cc
@@ -9,6 +9,8 @@
 #include <QRgb>
 #include "MainWindow.hh"
 
+//  std::cout << "_checkBox: " << qPrintable(tChecked) << std::endl;
+
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 MainWindow::MainWindow(QWidget *aParent)
@@ -32,7 +34,6 @@ MainWindow::~MainWindow()
 //-----------------------------------------------------------------------------
 void MainWindow::onSliderValueChanged(int aValue)
 {
-  std::cout << "Updating with value " << aValue << std::endl;
   updateCopyImage(aValue);
 }
 
@@ -42,25 +43,9 @@ void MainWindow::setupConnections()
 {
   std::cout << "Setting up connections" << std::endl;
 
-//  QObject::connect(_Slider, SIGNAL(valueChanged(int)),
   connect(_Slider, SIGNAL(valueChanged(int)),
-      this, SLOT(onSliderValueChanged(int)) );
+      this, SLOT(onSliderValueChanged(int)));
 }
-
-#if 0
-void MainWindow::onSend()
-{
-  std::cout << "onSend" << std::endl;
-  QString tChecked = "false";
-  if (_checkBox->checkState() == Qt::Checked)
-  {
-    tChecked = "true";
-  }
-  std::cout << "_checkBox: " << qPrintable(tChecked) << std::endl;
-  std::cout << "_lineEdit1: " << qPrintable(_lineEdit1->text()) << std::endl;
-  std::cout << "_lineEdit2: " << qPrintable(_lineEdit2->text()) << std::endl;
-}
-#endif
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
@@ -93,14 +78,9 @@ void MainWindow::updateCopyImage(int aValue)
     {
       QRgb tColor = _OriginalImage->pixel(i,j);
       int tGray = qGray(tColor);
-//      std::cout << "tGray, aValue: " << tGray << "," << aValue << std::endl;
-
-//      if ( tGray <= 25)
-//      if ( tGray <= 171)
       if ( tGray <= aValue)
       {
         tImage.setPixel(i,j,Qt::black);
-//      std::cout << "SETTING PIXEL" << std::endl;
       }
     }
 
@@ -114,9 +94,15 @@ void MainWindow::setupView()
 {
   std::cout << "setupView" << std::endl;
 
+  /*
+   * Load the original image, and make a copy.
+   */
   loadOriginalImage();
   _CopyImage = new QImage(_OriginalImage->copy());
 
+  /*
+   * Create the images widget.
+   */
   QWidget *tImagesWidget = new QWidget(this);
 
   QHBoxLayout *tImagesLayout = new QHBoxLayout(tImagesWidget);
@@ -133,7 +119,9 @@ void MainWindow::setupView()
   _Label2->setPixmap(*_CopyPixmap);
   tImagesLayout->addWidget(_Label2);
 
-
+  /*
+   * Create the controls widget.
+   */
   QWidget *tControlsWidget = new QWidget(this);
 
   QHBoxLayout *tControlsLayout = new QHBoxLayout(tControlsWidget);
@@ -144,10 +132,9 @@ void MainWindow::setupView()
   _Slider->setTracking(true);
   tControlsLayout->addWidget(_Slider);
 
-  tControlsWidget->setLayout(tControlsLayout);
+//  tControlsWidget->setLayout(tControlsLayout);
 
   QVBoxLayout *tLayout = new QVBoxLayout(this);
-//  QHBoxLayout *tLayout = new QHBoxLayout(this);
   tLayout->addWidget(tImagesWidget);
   tLayout->addWidget(tControlsWidget);
 
@@ -155,11 +142,6 @@ void MainWindow::setupView()
   tLayout->addWidget(t3);
 
   setLayout(tLayout);
-
-//  updateCopyImage(220);
-//      if ( tGray <= 25)
-//      if ( tGray <= 171)
-//      if ( tGray <= 220)
 
   QSize tSize = _OriginalImage->size();
   std::cout << "Image size: " << tSize.width() << "," << tSize.height() << std::endl;
@@ -183,14 +165,6 @@ void MainWindow::setupView()
       {
         tColorCounts[tGray] = 1;
       }
-//
-////      if ( tGray <= 25)
-////      if ( tGray <= 171)
-//      if ( tGray <= aValue)
-//      {
-////        _OriginalImage->setPixel(i,j,Qt::black);
-//        _CopyImage->setPixel(i,j,Qt::black);
-//      }
     }
 
 std::cout << "Color count: " << tColorCounts.size() << std::endl;
@@ -206,212 +180,5 @@ std::cout << "Color count: " << tColorCounts.size() << std::endl;
 
   std::cout << "Saving altered image..." << std::endl;
 
-//  tImage.save("/home/bill/Downloads/modified.jpg");
-
-#if 0
-  QImage tImage("/home/bill/Downloads/unnamed.jpg");
-
-  QSize tSize = tImage.size();
-  std::cout << "Image size: " << tSize.width() << "," << tSize.height() << std::endl;
-
-  std::map<QRgb,int> tColorCounts;
-  std::map<QRgb,int>::iterator tIter;
-
-  for (int i = 0; i < tSize.width(); i++)
-    for (int j = 0; j < tSize.width(); j++)
-    {
-      QRgb tColor = tImage.pixel(i,j);
-
-      tIter = tColorCounts.find(tColor);
-
-      if (tIter != tColorCounts.end())
-      {
-        tColorCounts[tImage.pixel(i,j)]++;
-      }
-      else
-      {
-        tColorCounts[tImage.pixel(i,j)] = 1;
-      }
-    }
-
-std::cout << "Color count: " << tColorCounts.size() << std::endl;
-
-  for (tIter = tColorCounts.begin(); tIter != tColorCounts.end(); tIter++)
-  {
-    std::cout << "Color/count: "
-        << tIter->first << "/"
-        << tIter->second << std::endl;
-  }
-
-#endif
   setMinimumSize(800,800);
-#if 0
-#ifdef OLD
-  std::cout << "setupView" << std::endl;
-
-  _checkBox = new QCheckBox("CheckBox");
-
-  _label1 = new QLabel("SomeReallyReallyLongLabel",this);
-  _lineEdit1 = new QLineEdit();
-
-  _label2 = new QLabel("Label",this);
-  _lineEdit2 = new QLineEdit();
-
-  QGridLayout *tGridLayout = new QGridLayout(this);
-  tGridLayout->setRowStretch(1,0);
-  tGridLayout->setRowStretch(2,0);
-
-  tGridLayout->addWidget(_checkBox,0,0);
-  tGridLayout->addWidget(_label1,1,0);
-  tGridLayout->addWidget(_label2,2,0);
-
-  tGridLayout->addWidget(_lineEdit1,1,1);
-  tGridLayout->addWidget(_lineEdit2,2,1);
-
-  QWidget *tEditorsWidget = new QWidget(this);
-  tEditorsWidget->setLayout(tGridLayout);
-
-
-  QPushButton *tSendButton = new QPushButton(QString("Send"),this);
-  tSendButton->resize(100, 30);
-
-  QObject::connect( tSendButton, SIGNAL(clicked()), this, SLOT(onSend()) );
-
-  QHBoxLayout *tBoxLayout = new QHBoxLayout(this);
-  tBoxLayout->setStretch(0,0);
-  tBoxLayout->setStretch(1,1);
-  tBoxLayout->addWidget(tEditorsWidget);
-  tBoxLayout->addWidget(tSendButton);
-
-//  resize(200,120);
-
-  setLayout(tBoxLayout);
-#else
-  std::cout << "setupView" << std::endl;
-
-
-  const int ROWS = 6;
-  const int COLS = 10;
-  QFrame *tGrid[ROWS][COLS];
-
-  QString _Notes[ROWS][COLS];
-  for (int i = 0; i < ROWS; i++)
-    for (int j = 0; j < COLS; j++)
-    {
-      _Notes[0][0] = "x";
-    }
-  _Notes[0][0] = "E";
-  _Notes[0][1] = "F";
-  _Notes[0][2] = "f#";
-  _Notes[0][3] = "G";
-  _Notes[0][4] = "g#";
-  _Notes[0][5] = "A";
-  _Notes[0][6] = "a#";
-  _Notes[0][7] = "B";
-  _Notes[0][8] = "b#";
-  _Notes[0][9] = "C";
-
-  for (int i = 0; i < ROWS; i++)
-    for (int j = 0; j < COLS; j++)
-    {
-      QFrame *tFrame = new QFrame(this);
-      tFrame->setFrameShape(QFrame::Box);
-      tFrame->setLineWidth(2);
-      tFrame->setFrameStyle(QFrame::Box | QFrame::Plain);
-      if (i == 0 && j == 0)
-      {
-        tFrame->setContentsMargins(2,2,2,2);
-tFrame->setPalette(QPalette(Qt::blue));
-tFrame->setAutoFillBackground(true);
-      }
-      else if (i == 0)
-      {
-        tFrame->setContentsMargins(0,2,2,2);
-      }
-      else if (j == 0)
-      {
-        tFrame->setContentsMargins(2,0,2,2);
-      }
-      else
-      {
-        tFrame->setContentsMargins(0,0,2,2);
-      }
-tFrame->setSizePolicy(QSizePolicy::MinimumExpanding,
-                     QSizePolicy::MinimumExpanding);
-
-
-      tFrame->setMinimumSize(QSize(60,20));
-
-//      char buff[100];
-//      sprintf(buff,"hello %d,%d",i,j);
-      QLabel *tLabel = new QLabel(_Notes[i][j],tFrame);
-//      tLabel->setIndent(30);
-      tLabel->setAlignment(Qt::AlignVCenter | Qt::AlignRight | Qt::AlignAbsolute);
-//      tLabel->setAlignment(Qt::AlignLeft | Qt::AlignAbsolute);
-      tLabel->setPalette(QPalette(Qt::gray));
-      tLabel->setAutoFillBackground(true);
-tLabel->setMinimumWidth(60);
-tLabel->setContentsMargins(0,0,0,0);
-tLabel->setSizePolicy(QSizePolicy::MinimumExpanding,
-                     QSizePolicy::MinimumExpanding);
-
-QHBoxLayout *tBL = new QHBoxLayout(tFrame);
-tBL->addWidget(tLabel);
-
-      tGrid[i][j] = tFrame;
-    }
-
-
-//  _checkBox = new QCheckBox("CheckBox");
-//
-//  _label1 = new QLabel("SomeReallyReallyLongLabel",this);
-//  _lineEdit1 = new QLineEdit();
-//
-//  _label2 = new QLabel("Label",this);
-//  _lineEdit2 = new QLineEdit();
-
-  QGridLayout *tGridLayout = new QGridLayout(this);
-  tGridLayout->setHorizontalSpacing(0);
-  tGridLayout->setVerticalSpacing(0);
-tGridLayout->setContentsMargins(0,0,0,0);
-
-  for (int i = 0; i < ROWS; i++)
-    for (int j = 0; j < COLS; j++)
-    {
-      tGridLayout->addWidget(tGrid[i][j],i,j);
-    }
-//  tGridLayout->setRowStretch(1,0);
-//  tGridLayout->setRowStretch(2,0);
-//
-//  tGridLayout->addWidget(_checkBox,0,0);
-//  tGridLayout->addWidget(_label1,1,0);
-//  tGridLayout->addWidget(_label2,2,0);
-//
-//  tGridLayout->addWidget(_lineEdit1,1,1);
-//  tGridLayout->addWidget(_lineEdit2,2,1);
-//
-//  QWidget *tEditorsWidget = new QWidget(this);
-//  tEditorsWidget->setLayout(tGridLayout);
-//
-//
-//  QPushButton *tSendButton = new QPushButton(QString("Send"),this);
-//  tSendButton->resize(100, 30);
-//
-//  QObject::connect( tSendButton, SIGNAL(clicked()), this, SLOT(onSend()) );
-
-  QWidget *tEditorsWidget = new QWidget(this);
-  tEditorsWidget->setLayout(tGridLayout);
-
-  QHBoxLayout *tBoxLayout = new QHBoxLayout(this);
-//  tBoxLayout->setStretch(0,0);
-//  tBoxLayout->setStretch(1,1);
-//  tBoxLayout->addWidget(tEditorsWidget);
-//  tBoxLayout->addWidget(tSendButton);
-  tBoxLayout->addWidget(tEditorsWidget);
-
-//  resize(200,120);
-
-  setLayout(tBoxLayout);
-#endif
-#endif
 }
